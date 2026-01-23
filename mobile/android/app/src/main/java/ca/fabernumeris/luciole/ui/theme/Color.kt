20667ffa5e9d57b4c3685833f5c65d9e6a17f668
Rafael de Andrade Sousa <53796+rafaelsousa@users.noreply.github.com>
2026-01-23 09:07:20 -0500

    Add components and theme to login screen (#18)

--- mobile/android/app/src/main/java/ca/fabernumeris/luciole/ui/theme/Color.kt
+++ mobile/android/app/src/main/java/ca/fabernumeris/luciole/ui/theme/Color.kt
@@ -2,10 +2,7 @@ package ca.fabernumeris.luciole.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val NightBlue = Color(0xFF0B1C2D)
+val FireflyGreen = Color(0xFF6BCF9D)
+val FireflyYellow = Color(0xFFFFE066)
+val SoftBlue = Color(0xFF4DA3FF)
